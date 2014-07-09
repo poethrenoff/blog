d89f821d99c5eac2be22c2bc28d479924be12193
Константинов Дмитрий <poethrenoff@gmail.com>
2014-07-09 09:14:28 +0400

    Модуль Портфолио на модели

--- htdocs/include/Adminko/Module/PortfolioModule.php
+++ htdocs/include/Adminko/Module/PortfolioModule.php
@@ -3,6 +3,7 @@ namespace Adminko\Module;
 
 use Adminko\Paginator;
 use Adminko\Db\Db;
+use Adminko\Model\Model;
 
 class PortfolioModule extends Module
 {
@@ -13,31 +14,17 @@ class PortfolioModule extends Module
         $cols_per_page = max(intval($this->getParam('cols_per_page_photo')), 1);
 
         $items_per_page = $rows_per_page * $cols_per_page;
-
-        $portfolio_query = 'select count(*) as _portfolio_count from portfolio where portfolio_active = 1';
-        $portfolio_count = Db::selectRow($portfolio_query);
-
-        if ($portfolio_count = $portfolio_count['_portfolio_count']) {
+        
+        $model_portfolio = Model::factory('portfolio');
+        $portfolio_count = $model_portfolio->getCount(array('portfolio_active' => 1));
+        
+        if ($portfolio_count) {
             $pages = Paginator::create($portfolio_count, array('by_page' => $items_per_page));
 
-            $portfolio_query = 'select * from portfolio where portfolio_active = 1
-				order by portfolio_order limit ' . $pages['by_page'] . ' offset ' . $pages['offset'];
-            $portfolio_list = Db::selectAll($portfolio_query);
+            $portfolio_list = $model_portfolio->getList(array('portfolio_active' => 1),
+                array('portfolio_order' => 'asc'), $pages['by_page'], $pages['offset']);
 
-            $portfolio_table = array();
-            for ($i = 0; $i < ceil(count($portfolio_list) / $cols_per_page); $i++) {
-                for ($j = 0; $j < $cols_per_page; $j++) {
-                    if (isset($portfolio_list[$i * $cols_per_page + $j])) {
-                        $portfolio_table[$i][$j] = $portfolio_list[$i * $cols_per_page + $j];
-                    } else {
-                        $portfolio_table[$i][$j] = array();
-                    }
-                }
-            }
-
-            $this->view->assign('portfolio_table', $portfolio_table);
-            $this->view->assign('table_cell_width', round(100 / $cols_per_page));
-
+            $this->view->assign('portfolio_list', $portfolio_list);
             $this->view->assign('pages', Paginator::fetch($pages));
         }
 
